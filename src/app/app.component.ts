98a9aa58186ee9a4ba5c567116aff9f16300e802
Yaroslav Oshyyko <yaroslav.oshiyko.5@gmail.com>
2016-09-09 00:20:07 +0300

    Changed sass to scss Added skeleton to home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HomePageComponent } from './homePageComponent/home-page.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss'],
+  directives: [HomePageComponent]
 })
 export class AppComponent {
   title = 'app works!';
