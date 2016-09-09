5b3affef2e7de30cded7047bd25a74d948aa13c8
Yaroslav Oshyyko <yaroslav.oshiyko.5@gmail.com>
2016-09-09 13:27:20 +0300

    Added root routing support

---
+++ src/app/app.routing.ts
@@ -0,0 +1,17 @@
+import {Routes, RouterModule} from '@angular/router';
+import {HomePageComponent} from "./homePageComponent/home-page.component";
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+    pathMatch: 'full' 
+  }
+
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
